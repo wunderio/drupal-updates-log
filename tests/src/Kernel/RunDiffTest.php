f63971c56839747dbad8ad282dacbc28cd04d141
Ragnar Kurm <ragnar.kurm@wunder.io>
2023-02-23 17:44:50 +0200

    Handle core bug: sometimes Drupal is not able to fetch module versions.

---
+++ tests/src/Kernel/RunDiffTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace tests\src\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\updates_log\UpdatesLog;
+
+/**
+ * Test diff handling.
+ *
+ * @group updates_log
+ */
+class RunDiffTest extends KernelTestBase {
+
+  /**
+   * The UpdatesLog service.
+   *
+   * @var \Drupal\updates_log\UpdatesLog
+   */
+  private UpdatesLog $updatesLogService;
+
+  /**
+   * The modules to load to run the test.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'update',
+    'updates_log',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->installConfig(['updates_log']);
+    $this->updatesLogService = \Drupal::service('updates_log.updates_logger');
+  }
+
+  /**
+   * @covers ::runDiff
+   */
+  public function testNoChanges(): void {
+
+    // Old.
+    $statuses_old = ['projX' => 'a'];
+    \Drupal::state()->set(
+        UpdatesLog::STATUSES_STATE,
+        $statuses_old
+    );
+
+    // New.
+    $statuses_new = ['projX' => ['status' => 'a']];
+
+    $this->updatesLogService->runDiff($statuses_new);
+
+    $statuses_updated = \Drupal::state()->get(
+        UpdatesLog::STATUSES_STATE
+    );
+
+    $this->assertEquals($statuses_old, $statuses_updated);
+  }
+
+  /**
+   * @covers ::runDiff
+   */
+  public function testYesChanges(): void {
+
+    // Old.
+    $statuses_old = ['projX' => 'a'];
+    \Drupal::state()->set(
+        UpdatesLog::STATUSES_STATE,
+        $statuses_old
+    );
+
+    // New.
+    $statuses_new = ['projX' => ['status' => 'b']];
+
+    $this->updatesLogService->runDiff($statuses_new);
+
+    $statuses_updated = \Drupal::state()->get(
+        UpdatesLog::STATUSES_STATE
+    );
+
+    // Expected.
+    $statuses_expected = ['projX' => 'b'];
+
+    $this->assertEquals($statuses_expected, $statuses_updated);
+  }
+
+}
---
+++ tests/src/Kernel/RunStatisticsTest.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace tests\src\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\updates_log\UpdatesLog;
+
+/**
+ * Test statistics handling.
+ *
+ * @group updates_log
+ */
+class RunStatisticsTest extends KernelTestBase {
+
+  /**
+   * The UpdatesLog service.
+   *
+   * @var \Drupal\updates_log\UpdatesLog
+   */
+  private UpdatesLog $updatesLogService;
+
+  /**
+   * The modules to load to run the test.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'update',
+    'updates_log',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->installConfig(['updates_log']);
+    $this->updatesLogService = \Drupal::service('updates_log.updates_logger');
+  }
+
+  /**
+   * @covers ::runStatistics
+   */
+  public function testEnvCondition(): void {
+    \Drupal::state()->set(
+        UpdatesLog::STATISTICS_TIME_STATE,
+        time()
+    );
+    putenv('UPDATES_LOG_TEST=1');
+    $status = $this->updatesLogService->runStatistics([], time());
+    $this->assertNotEquals(1, $status);
+  }
+
+  /**
+   * @covers ::runStatistics
+   */
+  public function testTimeCondition(): void {
+    \Drupal::state()->set(
+        UpdatesLog::STATISTICS_TIME_STATE,
+        time() - (60 * 60 * 24)
+    );
+    putenv('UPDATES_LOG_TEST');
+    $status = $this->updatesLogService->runStatistics([], time());
+    $this->assertNotEquals(1, $status);
+  }
+
+  /**
+   * @covers ::runStatistics
+   */
+  public function testBothCondition(): void {
+    \Drupal::state()->set(
+        UpdatesLog::STATISTICS_TIME_STATE,
+        time() - (60 * 60 * 24)
+    );
+    putenv('UPDATES_LOG_TEST=1');
+    $status = $this->updatesLogService->runStatistics([], time());
+    $this->assertNotEquals(1, $status);
+  }
+
+  /**
+   * @covers ::runStatistics
+   */
+  public function testNoCondition(): void {
+    \Drupal::state()->set(
+        UpdatesLog::STATISTICS_TIME_STATE,
+        time()
+    );
+    putenv('UPDATES_LOG_TEST');
+    $status = $this->updatesLogService->runStatistics([], time());
+    $this->assertEquals(1, $status);
+  }
+
+  /**
+   * @covers ::runStatistics
+   */
+  public function testUnknown(): void {
+    \Drupal::state()->set(
+        UpdatesLog::STATISTICS_TIME_STATE,
+        time() - (60 * 60 * 24)
+    );
+    putenv('UPDATES_LOG_TEST=1');
+    $statuses = [
+      'projX' => [
+        'status' => '???',
+        'version_used' => '1.2.3',
+      ],
+    ];
+    $status = $this->updatesLogService->runStatistics($statuses, time());
+    $this->assertEquals(2, $status);
+  }
+
+  /**
+   * @covers ::runStatistics
+   */
+  public function testNotUnknown(): void {
+    \Drupal::state()->set(
+        UpdatesLog::STATISTICS_TIME_STATE,
+        time() - (60 * 60 * 24)
+    );
+    putenv('UPDATES_LOG_TEST=1');
+    $statuses = [
+      'projX' => [
+        'status' => 'CURRENT',
+        'version_used' => '1.2.3',
+      ],
+    ];
+    $status = $this->updatesLogService->runStatistics($statuses, time());
+    $this->assertNotEquals(2, $status);
+  }
+
+  /**
+   * @covers ::runStatistics
+   */
+  public function testSuccess(): void {
+    $time = time();
+    \Drupal::state()->set(
+        UpdatesLog::STATISTICS_TIME_STATE,
+        $time - (60 * 60 * 24)
+    );
+    putenv('UPDATES_LOG_TEST=1');
+    $statuses = [
+      'projX' => [
+        'status' => 'CURRENT',
+        'version_used' => '1.2.3',
+      ],
+    ];
+    $status = $this->updatesLogService->runStatistics($statuses, $time);
+    $this->assertEquals(0, $status);
+
+    $time_updated = \Drupal::state()->get(
+        UpdatesLog::STATISTICS_TIME_STATE
+    );
+    $this->assertEquals($time, $time_updated);
+
+  }
+
+}
