eb975671e6684b05c813a5b9aeff98ef5bfbe39f
Juhani Moilanen <juhani.moilanen@wunder.io>
2023-04-20 13:46:36 +0300

    Fix "disabled" test

--- tests/src/Kernel/UpdatesLogDisabledTest.php
+++ tests/src/Kernel/UpdatesLogDisabledTest.php
@@ -48,13 +48,15 @@ class UpdatesLogDisabledTest extends KernelTestBase {
     $this->installSchema('dblog', ['watchdog']);
     $this->updatesLogService = \Drupal::service('updates_log.updates_logger');
     $this->db = \Drupal::database();
+    putenv("UPDATES_LOG_TEST");
+
   }
 
   /**
    * Test that there is no output when disabled = TRUE.
    */
   public function testDisabledDoesNotRun(): void {
-    new Settings(['updates_log_disabled' => TRUE]);
+    new Settings(['updates_log_disabled' => TRUE, 'hash_salt' => 'notsosecurehash']);
     $this->updatesLogService->run();
     $query = $this->db->query("select * from {watchdog}");
     $result = $query->fetchAll();
